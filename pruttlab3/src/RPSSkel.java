04804d1333965981a9d8feba8f6a7e1e93894406
roshult <roshult89@gmail.com>
2014-05-06 12:13:57 +0200

    amm

--- pruttlab3/src/RPSSkel.java
+++ pruttlab3/src/RPSSkel.java
@@ -1,10 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
-<<<<<<< HEAD
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-=======
->>>>>>> branch 'master' of https://github.com/EvertLagerberg/pruttlab3
 
 import javax.swing.*;
 
@@ -12,36 +9,23 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 class RPSSkel extends JFrame implements ActionListener {
-	
-	
     Gameboard myboard, computersboard;
     int counter; // To count ONE ... TWO  and on THREE you play
     Socket socket;
     BufferedReader in;
     PrintWriter out;
     JButton closebutton;
-    
+
     RPSSkel () {
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	closebutton = new JButton("Close");
-<<<<<<< HEAD
 	myboard = new Gameboard("Myself", this); // Must be changed later!
-=======
-	myboard = new Gameboard("Myself", this);
->>>>>>> branch 'master' of https://github.com/EvertLagerberg/pruttlab3
 	computersboard = new Gameboard("Computer");
 	JPanel boards = new JPanel();
 	boards.setLayout(new GridLayout(1,2));
 	boards.add(myboard);
 	boards.add(computersboard);
 	add(boards, BorderLayout.CENTER);
-	closebutton.addActionListener(new ActionListener(){
-		public void actionPerformed(ActionEvent e){
-			System.out.println("tja");
-			
-		}
-		
-	});
 	add(closebutton, BorderLayout.SOUTH);
 	setSize(300, 550);
 	setVisible(true);
@@ -55,38 +39,14 @@ class RPSSkel extends JFrame implements ActionListener {
     	});
     
     }
-<<<<<<< HEAD
     public static void main (String[] u) {
-=======
-
-    public static void main (String[] u) throws IOException {
->>>>>>> branch 'master' of https://github.com/EvertLagerberg/pruttlab3
 	new RPSSkel();
-	Klient klient = new Klient();
-	klient.Send("Evert");
-	klient.Read();
-	klient.Send("Sax");
-	klient.Read();
-	klient.Send("Pase");
-	klient.Read();
-	
-	
     }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println(e.getActionCommand());
-<<<<<<< HEAD
 		
-=======
->>>>>>> branch 'master' of https://github.com/EvertLagerberg/pruttlab3
 	}
 	
-<<<<<<< HEAD
-=======
-	
-	
-	
-
->>>>>>> branch 'master' of https://github.com/EvertLagerberg/pruttlab3
 }
