8cdcd0a67b41af39a465ff468419dcee7c1cfc77
EvertLagerberg <evert.lagerberg@gmail.com>
2014-05-06 12:07:35 +0200

    Server

--- pruttlab3/src/RPSSkel.java
+++ pruttlab3/src/RPSSkel.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -8,35 +7,58 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 class RPSSkel extends JFrame implements ActionListener {
+	
+	
     Gameboard myboard, computersboard;
     int counter; // To count ONE ... TWO  and on THREE you play
     Socket socket;
     BufferedReader in;
     PrintWriter out;
     JButton closebutton;
-
+    
     RPSSkel () {
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	closebutton = new JButton("Close");
-	myboard = new Gameboard("Myself"); // Must be changed later!
+	myboard = new Gameboard("Myself", this);
 	computersboard = new Gameboard("Computer");
 	JPanel boards = new JPanel();
 	boards.setLayout(new GridLayout(1,2));
 	boards.add(myboard);
 	boards.add(computersboard);
 	add(boards, BorderLayout.CENTER);
+	closebutton.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e){
+			System.out.println("tja");
+			
+		}
+		
+	});
 	add(closebutton, BorderLayout.SOUTH);
 	setSize(300, 550);
 	setVisible(true);
 
     }
 
-    public static void main (String[] u) {
+    public static void main (String[] u) throws IOException {
 	new RPSSkel();
+	Klient klient = new Klient();
+	klient.Send("Evert");
+	klient.Read();
+	klient.Send("Sax");
+	klient.Read();
+	klient.Send("Pase");
+	klient.Read();
+	
+	
     }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("hej");
+		System.out.println(e.getActionCommand());
 	}
+	
+	
+	
+	
+
 }
